1277e7746ec59c87f85bf79c3e1b31b5b9f20e45
SuperRama <superrama86@gmail.com>
2025-04-18 15:05:10 +0000

    update du script

--- validf.py
+++ validf.py
@@ -1,2 +1,14 @@
-# from Bio import seqIO
-dna = "ATCG"
+from Bio import SeqIO
+
+# Valid sequences
+seq_nucleotide = set("ATCG")
+
+for record in SeqIO.parse("seqexample.fasta", "fasta-blast"):
+    # Regarder dans la séquences
+    sequence = str(record.seq).upper()
+
+# Check if the sequences
+    if set(sequence).issubset(seq_nucleotide):
+        print(f"{record.id} : Séquence VALIDE")
+    else:
+        print(f"{record.id} : Séquence NON VALIDE")
